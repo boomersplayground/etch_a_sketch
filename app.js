6dbe90b0d51874aed7843f38a25c37bc1f72b16e
boomer <boomersplayground@gmail.com>
2022-06-04 10:51:11 -0700

    added dynamic size of wrapper.  Also removed and reinserted wrapper each time so they didnt stack on each other

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const sizeOfGrid = 16
+const resetButton = document.querySelector('button')
 const container = document.querySelector('.container')
 
 const createGrid = (amtOfGrids) => {
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('wrapper')
+
   for (let i = 0; i < amtOfGrids; i++) {
     const row = document.createElement('div')
     row.classList.add('grid-row')
 
     for (let j = 0; j < amtOfGrids; j++) {
-      const widthAndHeight = 960 / sizeOfGrid
+      const widthAndHeight = 960 / amtOfGrids
       const gridBox = document.createElement('div')
       gridBox.classList.add('grid-box')
       gridBox.style.width = `${widthAndHeight}px`
@@ -19,8 +22,26 @@ const createGrid = (amtOfGrids) => {
       row.appendChild(gridBox)
     }
 
-    container.appendChild(row)
+    wrapper.appendChild(row)
   } 
+  container.appendChild(wrapper)
 }
 
-createGrid(sizeOfGrid)
+resetButton.addEventListener('click', () => {
+  let userSize = Number(prompt('what dimensions do you want for the new grid'))
+
+  while (userSize > 100) {
+    userSize = Number(prompt('Pick a smaller number and make sure its 100 or less'))
+  }
+
+  const wrapper = document.querySelector('.wrapper')
+
+  if (!wrapper) {
+    createGrid(userSize)
+  } else {
+    wrapper.remove()
+    createGrid(userSize)
+  }
+
+})
+
